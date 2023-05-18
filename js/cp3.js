c9ba755ed46bfdd20ff6b5726527d7ec93f91d45
JDubas <Rm76153@fiap.com.br>
2023-05-18 20:27:38 -0300

    Melhorias no Email

--- js/cp3.js
+++ js/cp3.js
@@ -4,20 +4,33 @@ const inputEmail = document.querySelector("#");
 const inputSenha = document.querySelector("#");
 const inputSenha2 = document.querySelector("#");
 
+const erroNome = document.querySelector("#");
+const erroSobrenome = document.querySelector("#");
+const erroEmail = document.querySelector("#");
+const erroSenha = document.querySelector("#");
+const erroSenha2 = document.querySelector("#");
+
 
 
 inputEmail.addEventListener("blur", ()=>{
     let texto = inputEmail.value;
     if (input.value === "") {
         //Campo vazio
-        console.log("O campo está vazio");
+        erroNome.textContent = "* O email nao pode estar vazio"
       } else{
+        if(texto.value.length < 5){
+            erroNome.textContent = "* O email precisa ter 5 ou mais caracteres"  
+        }
         if (texto.includes("@")) {
             console.log("O texto contém o caractere @");
+            erroNome.textContent = "* O email precisa ser um email valido (Conter @)"  
           } else {
             //Campo sem @
-            console.log("O texto não contém o caractere @");
+            erroNome.textContent = ""
           }
       }
+
+
+
 });
 
